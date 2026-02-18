bb9bfca03c451adb5f228043dd80408bd930a49c
7H3LaughingMan <7H3LaughingMan@proton.me>
2026-02-18 14:30:07 -0600

    Add Exports

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,13 +1,6 @@
-import { ActorPF2e, ItemPF2e, MacroPF2e, ScenePF2e, UserPF2e } from "@7h3laughingman/pf2e-types";
-
-import CompendiumCollection = foundry.documents.collections.CompendiumCollection;
+import { ActorPF2e, UserPF2e } from "@7h3laughingman/pf2e-types";
 
 declare module "@7h3laughingman/foundry-helpers/utilities" {
-    function isCompendiumPack<T extends ActorPF2e<null>>(pack: unknown, type: "Actor"): pack is CompendiumCollection<T>;
-    function isCompendiumPack<T extends ItemPF2e<null>>(pack: unknown, type: "Item"): pack is CompendiumCollection<T>;
-    function isCompendiumPack<T extends MacroPF2e>(pack: unknown, type: "Macro"): pack is CompendiumCollection<T>;
-    function isCompendiumPack<T extends ScenePF2e>(pack: unknown, type: "Scene"): pack is CompendiumCollection<T>;
-
     function getCurrentUser(): UserPF2e;
     function userIsGM(user?: UserPF2e): boolean;
     function getPrimaryUpdater(actor: ActorPF2e): UserPF2e | null;
@@ -18,4 +11,7 @@ declare module "@7h3laughingman/foundry-helpers/utilities" {
 
 export * from "@7h3laughingman/foundry-helpers/utilities";
 
+export * from "./check.js";
 export * from "./damage.js";
+export * from "./roll.js";
+export * from "./system.js";
